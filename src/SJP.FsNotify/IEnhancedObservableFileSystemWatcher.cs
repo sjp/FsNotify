edbee3e17718bf06211ca50e2eacd5e6b29bafa0
Simon Potter <simon@sjp.co.nz>
2017-08-10 22:20:15 -0400

    Add interface for enhanced observable file system notifications.

---
+++ src/SJP.FsNotify/IEnhancedObservableFileSystemWatcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace SJP.FsNotify
+{
+    public interface IEnhancedObservableFileSystemWatcher : IObservableFileSystemWatcher
+    {
+        IObservable<FileSystemEventArgs> CreationTimeChanged { get; }
+
+        IObservable<FileSystemEventArgs> DirectoryNameChanged { get; }
+
+        IObservable<FileSystemEventArgs> FileNameChanged { get; }
+
+        IObservable<FileSystemEventArgs> LastAccessChanged { get; }
+
+        IObservable<FileSystemEventArgs> LastWriteChanged { get; }
+
+        IObservable<FileSystemEventArgs> SecurityChanged { get; }
+
+        IObservable<FileSystemEventArgs> SizeChanged { get; }
+    }
+}
